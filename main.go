a3abdc02b2ddf03b91ae8d292e8ac4501738dc43
carterthayer@protonmail.com <carterthayer@protonmail.com>
2021-01-19 22:25:34 -0500

    added

--- main.go
+++ main.go
@@ -1,37 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"flag"
 
-	"github.com/dixonwille/wmenu/v5"
+	"github.com/faith/color"
 )
 
 func main() {
-	menu := wmenu.NewMenu("Select an option : ")
-	//	menu.Action(func(opts []wmenu.Opt) error { fmt.Printf(opts[0].Text + " is your favorite food."); return nil })
-	menu.Option("Who-is Look up", nil, true, func(opt wmenu.Opt) error {
-		// whois()
-		// return nil
-		//	cmd := exec.Command("whois.go")
-		return nil
-	})
-	menu.Option("Look up", nil, true, nil)
-	menu.Option("ETC", nil, true, nil)
-	menu.Option("Reverse DNS Look up", nil, false, nil)
-	menu.Option("Tacos", nil, false, func(opt wmenu.Opt) error {
-		fmt.Printf("Tacos are great")
-		return nil
-	})
-	err := menu.Run()
-	if err != nil {
-		log.Fatal(err)
+	var banner = `
+				--------\
+				|
+				|
+				|
+				|
+				|
+	`
+	color.Cyan("%s", banner)
+	color.Red("          message")
+	url := flag.String("url", "u", "-url")
+	flag.Parse()
+	if *url != "u" {
+		color.Green("PWNScan Options :: \n")
+		color.Red("[01] Simple Nmap Scan")
+		color.Red("[02] Advanced Nmap Scan")
+		color.Red("[03] Who-is Lookup")
+		color.Red("[04] DNS Lookup")
+		color.Red("[05] Reverse DNS Lookup")
+		color.Red("[06] ETC")
+		color.Red("[07] ETC")
+		color.Red("[08] ETC")
+		color.Red("[09] ETC")
+		color.Red("[10] ETC")
+
+	var ch int 
+	color.Green("Select an option : ")
+	fmt.Scanf("%d".&ch)
+	if (ch == 1){
+		whois(*url)
 	}
-
+	}
 }
-
-//func whois() {
-//	cmnd := exec.Command("whois.go", "arg")
-//	//cmnd.Run() // and wait
-//	cmnd.Start()
-//}
